a5c3af8caf3aee9115556e8ebb7c30332acafdf2
Nguyen Tran Nhat Truong <jntntruong1495@gmail.com>
2025-10-04 10:27:02 +0700

    Initial commit

---
+++ src/main/java/com/nhattruong/__java_spring/LaoptopshopApplication.java
@@ -0,0 +1,13 @@
+package com.nhattruong.__java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaoptopshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaoptopshopApplication.class, args);
+	}
+
+}
